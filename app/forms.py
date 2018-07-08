1e88a80ce4383bd74e1071851f2656497b9eb9eb
Victor <victorc.galvao@gmail.com>
2018-07-08 13:11:37 -0300

    correção no login e cadastro de usuários

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,29 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
+
 
 class LoginForm(FlaskForm):
     username = StringField('Usuario', validators=[DataRequired()])
     password = PasswordField('Senha', validators=[DataRequired()])
     remember_me = BooleanField('Lembre-se de mim')
     submit = SubmitField('Entrar')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Nome de usuario', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Senha', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repita a senha', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Cadastrar')
+
+def validate_username(self, username):
+    user = User.query.filter_by(username=username.data).first()
+    if user is not None:
+        raise ValidationError('Por favor, use um nome de usuario diferente.')
+
+def validate_email(self, email):
+    if user is not None:
+        user = User.query.filter_by(email=email.data).first()
+        raise ValidationError('Por favor, use um e-mail diferente.')
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,21 @@
 from flask import render_template, flash, redirect, url_for
+from flask_login import logout_user
+from flask_login import current_user, login_user
+from flask_login import login_required
+from flask import request
+from werkzeug.urls import url_parse
 from app import app
 from app.forms import LoginForm
+from app.models import User
+from app import db
+from app.forms import RegistrationForm
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    usuario = {'nome_usuario': 'Joaozinho'}
+    # usuario = {'nome_usuario': 'Joaozinho'}
     itens = [
         {
             'descricao': 'arduino uno',
@@ -17,18 +26,44 @@ def index():
             'unidade': 'UND'
         }
     ]
-    return render_template('index.html', title='Home', usuario=usuario, itens=itens)
-
+    return render_template('index.html', title='Home',itens=itens)
 
 # the flask default method is only GET
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    # When the browser sends the GET request to receive the web page with the form,
-    # this method is going to return False. If it sends the POST request and all
-    # validations pass, the method returns True.
-    if form.validate_on_submit():
-        flash('Login exigido para {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Voce foi cadastrado com sucesso')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
